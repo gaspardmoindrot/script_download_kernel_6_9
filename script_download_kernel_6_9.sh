2f2a63d18c39aa465b6abe89a62911f9913ed852
gaspardmoindrot <gaspardmoindrot@gmail.com>
2024-07-05 16:38:13 +0200

    Script download kernel 6.9

---
+++ script_download_kernel_6_9.sh
@@ -0,0 +1,4 @@
+wget https://kernel.ubuntu.com/mainline/v6.9/amd64/linux-headers-6.9.0-060900-generic_6.9.0-060900.202405122134_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.9/amd64/linux-headers-6.9.0-060900_6.9.0-060900.202405122134_all.deb
+wget https://kernel.ubuntu.com/mainline/v6.9/amd64/linux-image-unsigned-6.9.0-060900-generic_6.9.0-060900.202405122134_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.9/amd64/linux-modules-6.9.0-060900-generic_6.9.0-060900.202405122134_amd64.deb
